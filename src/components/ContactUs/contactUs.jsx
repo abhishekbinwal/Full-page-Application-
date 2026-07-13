c0cef5d00ce4b18ea5877b6e00207d6377440ca2
abhishekbinwal <abhishekbinwalwork@gmail.com>
2026-07-13 16:30:55 +0530

    src added

---
+++ src/components/ContactUs/contactUs.jsx
@@ -0,0 +1,136 @@
+export default function Contact() {
+  return (
+    <div className="relative flex items-top justify-center min-h-[700px] bg-white sm:items-center sm:pt-0">
+      <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <div className="mt-8 overflow-hidden">
+          <div className="grid grid-cols-1 md:grid-cols-2">
+            <div className="p-6 mr-2 bg-gray-100 sm:rounded-lg">
+              <h1 className="text-3xl sm:text-4xl text-gray-800 font-extrabold tracking-tight">
+                Get in touch:
+              </h1>
+              <p className="text-normal text-lg sm:text-xl font-medium text-gray-600 mt-2">
+                Fill in the form to start a conversation
+              </p>
+
+              <div className="flex items-center mt-8 text-gray-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                  />
+                </svg>
+
+                <div className="ml-4 text-md tracking-wide font-semibold w-40">
+                  Acme Inc, Street, State, Postal Code
+                </div>
+              </div>
+
+              <div className="flex items-center mt-4 text-gray-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z"
+                  />
+                </svg>
+
+                <div className="ml-4 text-md tracking-wide font-semibold w-40">
+                  +44 1234567890
+                </div>
+              </div>
+
+              <div className="flex items-center mt-2 text-gray-600">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+                  />
+                </svg>
+
+                <div className="ml-4 text-md tracking-wide font-semibold w-40">
+                  info@acme.org
+                </div>
+              </div>
+            </div>
+
+            <form className="p-6 flex flex-col justify-center">
+              <div className="flex flex-col">
+                <label for="name" className="hidden">
+                  Full Name
+                </label>
+                <input
+                  type="name"
+                  name="name"
+                  id="name"
+                  placeholder="Full Name"
+                  className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
+                />
+              </div>
+
+              <div className="flex flex-col mt-2">
+                <label for="email" className="hidden">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Email"
+                  className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
+                />
+              </div>
+
+              <div className="flex flex-col mt-2">
+                <label for="tel" className="hidden">
+                  Number
+                </label>
+                <input
+                  type="tel"
+                  name="tel"
+                  id="tel"
+                  placeholder="Telephone Number"
+                  className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="md:w-32 bg-orange-700 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-orange-600 transition ease-in-out duration-300"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
